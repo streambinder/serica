fa74e6e8497c2f9fbaad16dd5241c71f801c54d0
streambinder <posta@davidepucci.it>
2025-05-27 23:53:40 +0200

    feat: first dump

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11-alpine
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_ENV=production
+WORKDIR /app
+RUN apk add --no-cache gcc musl-dev libffi-dev
+COPY app.py .
+COPY gallery.html.j2 .
+RUN pip install --no-cache-dir Flask
+EXPOSE 5000
+CMD ["flask", "run"]
---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+from typing import Optional
+
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__, template_folder=os.getcwd())
+
+MEDIA_EXTENSIONS = (".jpg", ".jpeg", ".png", ".mp4", ".mov")
+
+
+def get_galleries() -> list[str]:
+    return [fdir for fdir in os.listdir("/data") if os.path.isdir(f"/data/{fdir}")]
+
+
+def get_gallery_images(gallery: Optional[str] = None) -> list[str]:
+    return [
+        f"{gallery or ''}/{fname}"
+        for fname in os.listdir(f"/data/{gallery or ''}")
+        if fname.lower().endswith(MEDIA_EXTENSIONS) and fname != "cover.jpg"
+    ]
+
+
+def partition_media(media: list[str]) -> list[list[str]]:
+    result = []
+    i = 0
+    pattern = [2, 2, 1]
+    while i < len(media):
+        for size in pattern:
+            if i >= len(media):
+                break
+            group = media[i : i + size]
+            result.append(group)
+            i += size
+    return result
+
+
+@app.route("/media")
+def media():
+    return send_file(f"/data/{request.args.get('name')}", mimetype="image")
+
+
+@app.route("/branding/<asset>")
+def branding(asset: str):
+    return send_file(f"/branding/{asset}", mimetype="image")
+
+
+@app.route("/<gallery>")
+def gallery(gallery: str):
+    return render_template(
+        "gallery.html.j2",
+        galleries=get_galleries(),
+        gallery_media=partition_media(get_gallery_images(gallery)),
+        gallery_name=gallery,
+    )
+
+
+@app.route("/")
+def index():
+    return render_template(
+        "gallery.html.j2",
+        galleries=get_galleries(),
+        gallery_media=partition_media(get_gallery_images()),
+        gallery_name="",
+    )
+
+
+if __name__ == "__main__":
+    app.run()
